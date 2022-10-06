e021dba58d4261ae931271debd2032e4b776af5f
maja <mariusz.marek.olszewski@gmail.com>
2022-10-06 23:41:52 +0200

    added main if and main function

--- sync.py
+++ sync.py
@@ -51,15 +51,16 @@ class SyncFolders(SaveLog):
         pass
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Async sync folders based on in parameters', prog="Sync folders",
+        epilog="""You can pass multiple instance of the parameters exept the -l/--logPath to handle multiple folders, 
+                parameters are grouped by order of input parameter in groups of 3 (source, dest, intercal) and the number of those "sets" needs to be even""")
 
-parser = argparse.ArgumentParser(description='Async sync folders based on in parameters', prog="Sync folders",
-    epilog="""You can pass multiple instance of the parameters exept the -l/--logPath to handle multiple folders, 
-              parameters are grouped by order of input parameter in groups of 3 (source, dest, intercal) and the number of those "sets" needs to be even""")
-
-parser.add_argument('-s', '--source',   action='append', help="source folder path",          type=validate_filepath_arg, required=True)
-parser.add_argument('-r', '--replica',  action='append', help="replica folder path",         type=validate_filepath_arg, required=True)
-parser.add_argument('-i', '--interval', action='append', help="source folder interval scan", type=int,          required=True)
-parser.add_argument('-l', '--logPath',  action='store',  help="folder for logs",             type=validate_filepath_arg, required=True)
+    parser.add_argument('-s', '--source',   action='append', help="source folder path",          type=validate_filepath_arg, required=True)
+    parser.add_argument('-r', '--replica',  action='append', help="replica folder path",         type=validate_filepath_arg, required=True)
+    parser.add_argument('-i', '--interval', action='append', help="source folder interval scan", type=int,          required=True)
+    parser.add_argument('-l', '--logPath',  action='store',  help="folder for logs",             type=validate_filepath_arg, required=True)
+    return parser
 
 def validateSets(inputArgs):        
     elementNames = {}    
@@ -67,20 +68,21 @@ def validateSets(inputArgs):
         if type(argValues) is list:
             elementNames[argName] = len(argValues)    
     if(sum(elementNames.values()) % 3 != 0 and sum(elementNames.values()) > 3): raise Exception("Sorry, number or sets of arguments is not even")
+if __name__ == "__main__":
+    
+    inputArgs   = main().parse_args()
+    validateSets(vars(inputArgs))
 
-inputArgs   = parser.parse_args()
-validateSets(vars(inputArgs))
+    logLocation = inputArgs.logPath
+    synSet      = {}
 
-logLocation = inputArgs.logPath
-synSet      = {}
-
-for index, sourceFilePath in enumerate(inputArgs.source):
-    synSet[sourceFilePath + inputArgs.replica[index]] = [sourceFilePath, inputArgs.replica[index], inputArgs.interval[index]]
+    for index, sourceFilePath in enumerate(inputArgs.source):
+        synSet[sourceFilePath + inputArgs.replica[index]] = [sourceFilePath, inputArgs.replica[index], inputArgs.interval[index]]
 
 
-asyncLoop = asyncio.get_event_loop()
-asyncTasks = []
-for job, jobParams in synSet.items():
-    folderSync = SyncFolders(jobParams)
-    asyncTasks.append(asyncLoop.create_task(folderSync.runSync()))
-asyncLoop.run_forever()
+    asyncLoop = asyncio.get_event_loop()
+    asyncTasks = []
+    for job, jobParams in synSet.items():
+        folderSync = SyncFolders(jobParams)
+        asyncTasks.append(asyncLoop.create_task(folderSync.runSync()))
+    asyncLoop.run_forever()
