54da4f4dbf93e3e92509d643b6f677b8478a9c3a
maja <mariusz.marek.olszewski@gmail.com>
2022-10-10 19:56:47 +0200

    added key listener code

--- sync.py
+++ sync.py
@@ -1,11 +1,38 @@
 from os import listdir
 from pathvalidate.argparse import validate_filepath_arg
+from threading import Thread
 
 import hashlib
 import shutil
 import argparse
 import time
 import pathlib
+import keyboard
+import pynput
+
+KeyComb_Quit = [
+    {pynput.keyboard.Key.ctrl, pynput.keyboard.KeyCode(char='q')},
+    {pynput.keyboard.Key.ctrl_l, pynput.keyboard.KeyCode(char='q')},
+    {pynput.keyboard.Key.ctrl_r, pynput.keyboard.KeyCode(char='q')}
+
+]
+
+def on_press(key):
+    global is_quit
+    if any([key in comb for comb in KeyComb_Quit]):
+        current.add(key)
+        if any(all(k in current for k in comb) for comb in KeyComb_Quit):
+            is_quit = True
+
+def on_release(key):
+    try:
+        current.remove(key)
+    except KeyError:
+        pass
+current = set()
+listener = pynput.keyboard.Listener(on_press=on_press, on_release=on_release)
+listener.start()
+
 
 class SaveLog:
     def __init__(self, logLocation):
@@ -17,35 +44,39 @@ class SaveLog:
         pass
     async def saveToFile(self):
         pass
-class SyncFolders(SaveLog):
+class SyncFolders(SaveLog, Thread):
     def __init__(self, Instructions):
+        Thread.__init__(self)
         self.sourceFilePath = Instructions[0]
         self.destinFilePath = Instructions[1]
         self.TimeInterval   = Instructions[2]        
         # sourceFilePath, inputArgs.replica[index], inputArgs.interval[index]
         super().__init__(logLocation)
-    def runSync(self):
+    def run(self):
+
+    def _runSync(self):
         while True:
                 self.scanFolder()
                 self.diff()
                 self.operationOfFolders()
                 print(f"working lol -> {self.TimeInterval}")                    
         
-    def scanFolder(self):
+    def _scanFolder(self):
         #scan the folders and list the files and run the diff command
         # add md5 hash to all of the files     
         pass
-    def diff(self):
+    def _diff(self):
         #create a new list 
         pass    
-    def operationOfFolders(self):
+    def _operationOfFolders(self):
         pass
 
 
 def main():
     parser = argparse.ArgumentParser(description='Async sync folders based on in parameters', prog="Sync folders",
         epilog="""You can pass multiple instance of the parameters exept the -l/--logPath to handle multiple folders, 
-                parameters are grouped by order of input parameter in groups of 3 (source, dest, intercal) and the number of those "sets" needs to be even""")
+                parameters are grouped by order of input parameter in groups of 3 (source, dest, intercal) and the number of those "sets" needs to be even
+                maximum of folders per process is around 500""")
 
     parser.add_argument('-s', '--source',   action='append', help="source folder path",          type=validate_filepath_arg, required=True)
     parser.add_argument('-r', '--replica',  action='append', help="replica folder path",         type=validate_filepath_arg, required=True)
