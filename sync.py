864b11e5d18d763e5c78a0acc930b2e81c5b664e
maja <mariusz.marek.olszewski@gmail.com>
2022-10-10 23:00:15 +0200

    finished first difference in folders

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ from pathvalidate.argparse import validate_filepath_arg
 from threading import Thread
 
 import hashlib, shutil, argparse, time, pathlib, glob
-import keyboard, pynput
+import keyboard, pynput, sys, os
 
 is_quit      = False
 quit_char    = 'b'
@@ -12,7 +12,6 @@ KeyComb_Quit = [
     {pynput.keyboard.Key.ctrl_l, pynput.keyboard.KeyCode(char=quit_char)},
     {pynput.keyboard.Key.ctrl_r, pynput.keyboard.KeyCode(char=quit_char)}
 ]
-
 def on_press(key):
     global is_quit
     if any([key in comb for comb in KeyComb_Quit]):
@@ -45,8 +44,9 @@ class SyncFolders(SaveLog, Thread):
         Thread.__init__(self)
         self.sourceFilePath = Instructions[0]
         self.destinFilePath = Instructions[1]
-        self.TimeInterval   = Instructions[2]
-        self.mapOfFiles     = {}
+        self.TimeInterval   = Instructions[2] # * 30
+        self.mapOfSouFiles  = {}
+        self.mapOfDesFiles  = {}
         self.diffMap        = {}
         
         super().__init__(saveLogLocation)
@@ -57,16 +57,36 @@ class SyncFolders(SaveLog, Thread):
         while True:
                 self._scanFolder()
                 self._diff()
-                self._operationOfFolders()
-                print(f"working lol -> {self.TimeInterval}")                    
+                self._operationOfFolders()                
+                time.sleep(self.TimeInterval)
                 if is_quit: break 
-        
+    def _extractMetaAndScan(self,scanRoot, folderMapped):
+        searchString =  os.path.join(scanRoot, "**", "**")
+        for files in glob.iglob(searchString, recursive=True):            
+            if files == os.path.join(scanRoot, "") : continue
+            fileLoc   = files
+            meta      = os.stat(files)
+            modTime   = meta.st_mtime
+            fileSize  = meta.st_size
+            fileLoc   = fileLoc.replace(scanRoot, "")
+
+            # print(f"{files} == {fileLoc}")
+
+            hashMD5   = hashlib.md5( (fileLoc + str(modTime) + str(fileSize)).encode('utf-8') ).hexdigest()
+            # hashMD5   = (fileLoc + ":" + str(modTime) + str(fileSize)).encode('utf-8') 
+            # print(f"fileLoc -> {fileLoc}")
+            folderMapped[hashMD5] = fileLoc
     def _scanFolder(self):
-        #scan the folders and list the files and run the diff command
-        # add md5 hash to all of the files     
-        pass
+        
+        self._extractMetaAndScan(self.sourceFilePath, self.mapOfSouFiles)        
+        self._extractMetaAndScan(self.destinFilePath, self.mapOfDesFiles)
+
     def _diff(self):
-        #create a new list 
+        # C =                             {k:v for k,v in A.items() if k not in B}
+        diffSourceRemoveDestinasion = {hashKey: dictValue for hashKey, dictValue in self.mapOfSouFiles.items() if hashKey not in self.mapOfDesFiles}
+        # diffSouDest = self.mapOfSouFiles - self.mapOfDesFiles
+        # print(f"mapOfSouFiles {self.mapOfSouFiles} \n mapOfDesFiles{self.mapOfDesFiles}")
+        print(f"diffSourceRemoveDestinasion -> {diffSourceRemoveDestinasion}")
         pass    
     def _operationOfFolders(self):
         pass
@@ -93,8 +113,7 @@ def main():
     for index, sourceFilePath in enumerate(inputArgs.source):
         synSet[sourceFilePath + inputArgs.replica[index]] = [sourceFilePath, inputArgs.replica[index], inputArgs.interval[index]]
     
-    for job, jobParams in synSet.items():
-        # print(f"job -> {job}, jobParams->{jobParams}")
+    for job, jobParams in synSet.items():        
         folderSync = SyncFolders(jobParams,logLocation)
         folderSync.run()
 
